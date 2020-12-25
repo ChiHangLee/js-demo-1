9d2c27620c961fd582f9b680229253b2d3c38804
chihanglee <lujirk240@gmail.com>
2020-12-25 15:42:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-var div = document.createElement('div')
+let div1 = document.createElement('div');
+div1.className = 'demo'
 
-document.body.appendChild('div')
+document.body.appendChild(div1);
+
+
+document.body.onclick = function (e) {
+    div1.style.top = e.clientX + 'px'
+    div1.style.left = e.clientY + 'px'
+}
