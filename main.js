595a161d9963eec560282572c55c7c790a3529ff
chihanglee <lujirk240@gmail.com>
2020-12-29 21:23:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,14 +17,22 @@ div1.onmousedown = function (e) {
 
 document.onmousemove = function (e) {
     if (dragging === true) {
-        console.log(lastX, lastY)
-        console.log(e.clientX, e.clientY)
         var deltaX = e.clientX - lastX
         var deltaY = e.clientY - lastY
         var top = parseInt(div1.style.top) || 0
         var left = parseInt(div1.style.left) || 0
+        var resultY = top + deltaY
+        var resultX = left + deltaX
+        if (resultY < 0){
+            resultY = 0
+        }
+        if(resultX < 0){
+            resultX = 0
+        }
         div1.style.top = top + deltaY + 'px'
         div1.style.left = left + deltaX + 'px'
+        lastX = e.clientX
+        lastY = e.clientY
     }
 }
 
